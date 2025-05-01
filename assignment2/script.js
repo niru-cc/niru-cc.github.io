8a1516ff0c9d4465b4b82ed4fa20887c195b1ee9
Sakura <s4088734@student.rmit.edu.au>
2025-05-01 11:02:59 +1000

    added mute function and consts

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,7 +1,16 @@
-const audio = document.querySelector("#musicplayer");
+const audio = document.querySelector("#music");
+console.log(audio);
 const playPauseBtn = document.querySelector("#play");
 const playPauseImg = document.querySelector("#playicon");
 const progressBar = document.querySelector("#progressbar");
+//added extra consts for added buttons
+const muteButton = document.querySelector("#mute");
+const muteIcon = document.querySelector("#muteicon");
+const skipButton = document.querySelector("#skip");
+const skipIcon = document.querySelector("#skipicon");
+const timerButton = document.querySelector("#timer");
+const timerIcon = document.querySelector("#timericon");
+
 audio.removeAttribute("controls");
 
 // playPauseBtn.addEventListener("click", togglePlayPause);
@@ -19,4 +28,16 @@ function updateProgressBar() {
   const value = (audio.currentTime / video.duration) * 100;
   progressBar.style.width = value + "%";
 }
-// Add other functionalities here
+
+muteButton.addEventListener("click", toggleMute);
+function toggleMute() {
+  if (audio.muted) {
+    audio.muted = false;
+    muteIcon.src = "https://img.icons8.com/ios-glyphs/24/high-volume--v1.png";
+  } else {
+    audio.muted = true;
+    muteIcon.src = "https://img.icons8.com/ios-glyphs/24/no-audio--v1.png";
+  }
+}
+
+// I added a function for setting up a timer so that the tracks wouldn't continue to play for too long after the user falls asleep.
